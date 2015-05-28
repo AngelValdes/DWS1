b68b19e14adf5380d88aeb031cef90bdddb0f4e5
Angel Valdes <AngelValdes@hotmail.com>
2015-05-28 11:15:01 -0400

    project 4

---
+++ Valdes-Angel-Project4/scripts/app.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    //get all radio buttons
+    var radios = document.getElementsByName("rate");
+    //connect the same function listener to all radio buttons click event
+    for (var i = 0; i < radios.length; i++) {
+        radios[i].addEventListener("click", function (source) {
+            if (source.currentTarget.id == "amazing") {
+                message.innerHTML = "Great choice!";
+            } else {
+                message.innerHTML = "Grrr...!";
+            }
+        });
+    }
+})();
